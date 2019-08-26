73b667e5a8dec6f432af9099c15b97589729ecd0
mahbubur.rahman <mahbubur.rahman@bjitgroup.com>
2019-08-26 15:07:22 +0900

    Added ace editor for c++

--- OnlineJudgeWeb/src/app/onlineide/onlineide.component.ts
+++ OnlineJudgeWeb/src/app/onlineide/onlineide.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild, OnInit, ElementRef } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import * as ace from 'ace-builds';
+import 'ace-builds/src-noconflict/mode-c_cpp';
+import 'ace-builds/src-noconflict/theme-github';
+
 import * as $ from 'jquery';
+
+const THEME = 'ace/theme/github'; 
+const LANG = 'ace/mode/c_cpp';
+
 @Component({
   selector: 'app-onlineide',
   templateUrl: './onlineide.component.html',
@@ -10,9 +18,25 @@ import * as $ from 'jquery';
 export class OnlineideComponent implements OnInit {
     new =null;
     codeCollectorForm;
+
+    @ViewChild('codeEditor') codeEditorElmRef: ElementRef;
+    private codeEditor: ace.Ace.Editor;
+
     constructor(private httpclient: HttpClient){
     }
+
 	ngOnInit() {
+		const element = this.codeEditorElmRef.nativeElement;
+        const editorOptions: Partial<ace.Ace.EditorOptions> = {
+            highlightActiveLine: true,
+            minLines: 23,
+            maxLines: 23,
+        };
+        this.codeEditor = ace.edit(element, editorOptions);
+        this.codeEditor.setTheme(THEME);
+        this.codeEditor.getSession().setMode(LANG);
+        this.codeEditor.setShowFoldWidgets(true); // for the scope fold feature
+	
 		this.codeCollectorForm = new FormGroup({
 		    value: new FormControl(""),
 		    input: new FormControl("")
@@ -26,9 +50,9 @@ export class OnlineideComponent implements OnInit {
 	onSubmit() {
 	    this.code=this.codeCollectorForm.value;
 		let formData: FormData = new FormData();	
-		formData.append('value', this.code.value);
-		formData.append('input', this.code.input);
-	    this.httpclient.post("http://localhost:8000/onlineide/runcode",formData).subscribe(
+		formData.append('value', this.codeEditor.getValue());
+		formData.append('input', this.code.input);	    
+		this.httpclient.post("http://localhost:8000/onlineide/runcode",formData).subscribe(
 	        (data) => {
 	        let  output = JSON.parse(JSON.stringify(data));
 	        $('#output').val(output.object);
